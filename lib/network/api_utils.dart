81b5bf07462c47899ccd81f47d453a89fce748b3
Chirag <vaghelacd99@gmail.com>
2025-08-22 19:33:06 +0530

    working on api

--- lib/network/api_utils.dart
+++ lib/network/api_utils.dart
@@ -2,58 +2,19 @@ class ApiPath {
   static const String startPoint = '/auth';
 
 
-  static const String sendOtpUrl = '$startPoint/auth/otp';
-  static const String verifyOtpUrl = '$startPoint/auth/verifyotp';
-  static const String signupUrl = '$startPoint/auth/signup';
-  static const String forgotPwdSendOtpUrl =
-      '$startPoint/auth/forgotpassword/otp';
-  static const String forgotPwdVerifyOtpUrl =
-      '$startPoint/auth/forgotpassword/verifyotp';
-  static const String forgotPwdUrl = '$startPoint/auth/forgotpassword';
   static const String loginUrl = '$startPoint/login';
-  static const String logoutUrl = '$startPoint/logout';
-  static const String feedUrl = '$startPoint/feed';
-  static const String feedPostViewsUrl = '$startPoint/feed/feedpostviews';
-  static const String getEnquiryTypesUrl = '$startPoint/dropdowns/enquirytypes';
-  static String getEnquiryPurposeUrl(String sId) =>
-      '$startPoint/dropdowns/enquirytypes/$sId/enquirypurposemasters';
-  static String getEnquiryChannelsUrl(String sId) =>
-      '$startPoint/dropdowns/enquirytypes/$sId/enquirypreferredchannels';
-  static const String enquiriesUrl = '$startPoint/enquiries';
-  static String enquiryDetailUrl(String sId) => '$startPoint/enquiries/$sId';
-  static String attachmentsUrl(String? sId) =>
-      '$startPoint/entity/$sId/attachments';
-  static String notesUrl(String? sId) => '$startPoint/entity/$sId/notes';
-  static String editNotesUrl(String? sId, String? noteSId) =>
-      '$startPoint/entity/$sId/notes/$noteSId';
-  static String getClaimReasons = '$startPoint/dropdowns/claimreasons';
-  static String getClaimBusinessTypes = '$startPoint/dropdowns/businesstypes';
-  static String claimsUrl = '$startPoint/claims';
-  static String claimDetailUrl(String? sId) =>
-      '$startPoint/claims/$sId/details';
-  static String editClaimUrl(String? sId) => '$startPoint/claims/$sId';
-  static String getClaimHistoryUrl(String? sId) =>
-      '$startPoint/claims/$sId/history';
-  static String truCastUrl = '$startPoint/trucast';
-  static String truDriveUrl = '$startPoint/TruDrive';
-  static String addFolderUrl = '$startPoint/TruDrive/EmptyFolder';
-  static String uploadTruDriveFileUrl =
-      '$startPoint/TruDrive/UploadDriveDocument';
-  static String deleteFileFolderUrl = '$startPoint/TruDrive/DeleteDocuments';
-  static String renameFileFolderUrl(String docSId) =>
-      '$startPoint/TruDrive/Rename/$docSId';
-  static String downloadDriveFolderUrl(String sId) =>
-      '$startPoint/TruDrive/$sId/Download';
-  static String getSupportTicketCategory =
-      '$startPoint/dropdowns/SupportTicketCategory';
-  static String addFeedbackUrl = '$startPoint/supporttickets';
-  static String notificationUrl = '$startPoint/notifications';
-  static String changePasswordUrl = '$startPoint/auth/changepassword';
-  static String getProfileUrl = '$startPoint/auth/profile';
-  static String sendUpdateProfileOtp = '$startPoint/auth/profile/otp';
-  static String updateProfileUrl = '$startPoint/auth/profile';
-  static String updateProfileImageUrl = '$startPoint/auth/profileimage';
-  static String deleteAccountUrl = '$startPoint/auth/deleteaccount';
+  static const String createCycleUrl = '/cycles';
+  static const String getCycleUrl = '/cycles';
+
+  // Seeds
+  static const String getSeedsUrl = '/seed';
+  static const String getSeedsBrandUrl = '/seed/brands';
+  static const String addSeedLotUrl = '/seed/lots';
+  static const String getSeedLotUrl = '/seed/lots';
+
+  // Cycles
+  static const String getCycle = '/cycles';
+
 }
 
 abstract interface class ApiUtils {
